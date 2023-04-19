f89cc523fc9dcaf9f9db9f75d6a9914712b18cc0
Joe Alt <joejoyalt@gmail.com>
2023-04-19 17:43:00 +0200

    install cors, mongoose, express-oauth2-jwt-bearer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const app = require('./server');
-const config = require('./config/config');
+const app = require('./server')
+const Config = require('./config/config')
+const connectDB = require('./db/connect')
 
-// SERVER WEB
-app.listen(config.app.PORT, () => {
-    console.log(`Server is running on port ${config.app.PORT}`);
-});
+connectDB().then(async function onServerInit () {
+  console.log('Database connected')
+
+  app.listen(Config.app.PORT, () => {
+    console.log('Server is running on port ' + Config.app.PORT)
+  })
+})
--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
-const albumRouter = require('./routes/albums.routes')
+const cors = require('cors')
+const userRoutes = require('./routes/user.routes')
+const todoRoutes = require('./routes/todo.routes')
 
 const app = express()
 
+app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
-
-app.use('/albums', albumRouter)
-
+app.use(express.json())
+app.use('/user', userRoutes)
+app.use('/todo', todoRoutes)
 
 module.exports = app
